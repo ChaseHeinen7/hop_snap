810b17611810d661da4ed355f75a027c6c64875a
Chase Heinen <71614692+ChaseHeinen7@users.noreply.github.com>
2021-04-28 16:16:19 -0500

    Updated sig file to generate significance and time values

---
+++ sig.py
@@ -0,0 +1,19 @@
+import numpy as np
+import datetime
+
+"""
+Generate random time series data from a detector.
+
+"""
+
+def siggenerate():
+        n = np.random.randint(3,12);
+        sigarray = np.around(np.random.normal(0,1,n), decimals=2)
+        timearray = [];
+        initial_time = datetime.datetime.now();
+        timearray.append(initial_time);
+        for i in range(n):
+             timearray.append(timearray[i] + datetime.timedelta(milliseconds = 5))
+        for i in range(len(timearray)):
+             timearray[i] = timearray[i].strftime("%y/%m/%d-%H:%M:%S:%f")
+        return sigarray, timearray
