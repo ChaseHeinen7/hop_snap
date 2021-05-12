91eb34d8f18ffceb466de374508d2ea06630d625
Chase Heinen <71614692+ChaseHeinen7@users.noreply.github.com>
2021-05-12 10:40:03 -0500

    Update sig.py

--- hop_snap/sig.py
+++ hop_snap/sig.py
@@ -7,13 +7,13 @@ Generate random time series data from a detector.
 """
 
 def siggenerate():
-        n = np.random.randint(3,12);
+        n = np.random.randint(8,12);
         sigarray = np.around(np.random.normal(0,1,n), decimals=2)
         timearray = [];
         initial_time = datetime.datetime.now();
         timearray.append(initial_time);
         for i in range(n):
-             timearray.append(timearray[i] + datetime.timedelta(milliseconds = 5))
+             timearray.append(timearray[i] + datetime.timedelta(milliseconds = 100))
         for i in range(len(timearray)):
              timearray[i] = timearray[i].strftime("%y/%m/%d-%H:%M:%S:%f")
         return sigarray, timearray
